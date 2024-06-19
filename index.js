7ed22c523d9425650e307edb3e01063f692bb626
Susan Kafle <kafle.susan671@outlook.com>
2024-06-19 08:58:42 +0600

    added folder structure and db connection

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectToDB = require('./database/db');
+
+// creating an express app
+const app = express();
+// configure dotenv to use .env
+dotenv.config();
+// Defining port
+const PORT = process.env.PORT || 5000;
+// running the server on port 5000
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
