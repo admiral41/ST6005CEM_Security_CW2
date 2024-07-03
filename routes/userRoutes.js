c978ca4fdde9cfccbcea5f8b6955e7768177a8bd
Susan Kafle <kafle.susan671@outlook.com>
2024-07-03 14:42:19 +0600

    added routes for the controller made

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -29,4 +29,31 @@ router.post(
     passwordMiddleware.checkPasswordExpiry, // Middleware to check if password is expired
     loginUser
 );
+
+router.post(
+    '/change-password',
+    authMiddleware,
+    [
+        body('newPassword')
+            .isLength({ min: 8, max: 12 }).withMessage('Password must be between 8 and 12 characters')
+            .matches(/[A-Z]/).withMessage('Password must contain an uppercase letter')
+            .matches(/\d/).withMessage('Password must contain a number')
+            .matches(/[!@#$%^&*]/).withMessage('Password must contain a special character')
+    ],
+    passwordMiddleware.checkPasswordHistory, // Middleware to check if the new password was used before
+    changePassword
+);
+
+router.get('/auth/check', authMiddleware, async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id).select('-password'); // Use _id
+        res.json({ user });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+router.get('/unlock/:resetToken', unlockAccount);
+router.get('/verify/:verificationToken', verifyEmail);
 module.exports = router;
