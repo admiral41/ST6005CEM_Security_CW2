f9f0f82c4ef4f239c2efaa3ab5634c46e195d8ff
Susan Kafle <kafle.susan671@outlook.com>
2024-07-12 16:36:49 +0600

    added middleware for password and auth

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,45 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+
+const authMiddleware = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({
+            success: false,
+            message: "Authorization header missing",
+        });
+    }
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: "Token missing",
+        });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded._id);
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "User not found",
+            });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({
+                success: false,
+                message: "Token expired",
+            });
+        }
+        res.status(401).json({
+            success: false,
+            message: "Invalid token",
+        });
+    }
+};
+
+module.exports = authMiddleware;
